3e81431db6256882172f9c4a5d43f476e6c73285
Renato Bressan <renatoikeda98@gmail.com>
2025-11-06 22:32:15 -0300

    Update helloworld.c

--- C/helloworld.c
+++ C/helloworld.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main() {
-  printf("Hello, World!");
+  printf("Hello, World!\n");
+  printf("(This code is for filling purposes only, just ignore it.)\n");
   return 0;
 }
